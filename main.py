0f8e46bb4bbd2123bbc9b82581ee38e5beb02021
Kai Anderson <kai@kaianderson.com>
2022-08-14 17:46:49 -0700

    nice

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
+from pdfminer.high_level import extract_text_to_fp
 
 output_string = StringIO()
+output_string2 = StringIO()
+
 with open('docs/contracts/groundfloor-sub.pdf', 'rb') as in_file:
     parser = PDFParser(in_file)
     doc = PDFDocument(parser)
@@ -17,4 +20,9 @@ with open('docs/contracts/groundfloor-sub.pdf', 'rb') as in_file:
     for page in PDFPage.create_pages(doc):
         interpreter.process_page(page)
 
-print(output_string.getvalue())
+# print(output_string.getvalue())
+
+with open ('docs/contracts/groundfloor-sub.pdf', 'rb') as fin:
+    extract_text_to_fp(fin, output_string2, laparams=LAParams(), output_type='html', codec=None)
+
+print(output_string2.getvalue())
