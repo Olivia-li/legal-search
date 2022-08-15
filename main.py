b47c615e908908597b60db46c9a9b9b0cb435b80
Olivia Li <oliviali.ouyang@gmail.com>
2022-08-14 19:46:20 -0700

    Get the text from pdf

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
-from io import BytesIO
+from io import BytesIO, StringIO
+from typing import Text
 
-from pdfminer.converter import HTMLConverter 
+from pdfminer.converter import HTMLConverter, TextConverter 
 from pdfminer.layout import LAParams
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
-output_string = BytesIO()
+output_html = BytesIO()
+output_string = StringIO()
+
 
 with open('docs/contracts/hotelier-sub.pdf', 'rb') as in_file:
     parser = PDFParser(in_file)
     doc = PDFDocument(parser)
     rsrcmgr = PDFResourceManager()
-    device = HTMLConverter(rsrcmgr, output_string, laparams=LAParams())
+    device = HTMLConverter(rsrcmgr, output_html, laparams=LAParams(line_overlap=0.1, line_margin=0.1, word_margin=0.05, char_margin=2.0))
+    string_device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
     interpreter = PDFPageInterpreter(rsrcmgr, device)
+    string_interpreter = PDFPageInterpreter(rsrcmgr, string_device)
+
     for page in PDFPage.create_pages(doc):
         interpreter.process_page(page)
 
-print(output_string.getvalue().decode('utf8').replace("<br>", "").replace("<body>", '<body style="text-align: justify">'))
+    for page in PDFPage.create_pages(doc):
+        string_interpreter.process_page(page)
+
+print(output_html.getvalue().decode('utf8').replace("<body>", '<body style="text-align: justify;">'))
+print(output_string.getvalue())
