895c584694a94a37b25e4b0c4b47ddedf6b3a6e1
Olivia Li <oliviali.ouyang@gmail.com>
2022-08-14 19:49:18 -0700

    make them functions

--- main.py
+++ main.py
@@ -8,24 +8,31 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
-output_html = BytesIO()
-output_string = StringIO()
 
+def convert_pdf_to_html(path: Text) -> Text:
+    output_html = BytesIO()
+    with open(path, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        device = HTMLConverter(rsrcmgr, output_html, laparams=LAParams(line_overlap=0.1, line_margin=0.1, word_margin=0.05, char_margin=2.0))
+        interpreter = PDFPageInterpreter(rsrcmgr, device)
 
-with open('docs/contracts/hotelier-sub.pdf', 'rb') as in_file:
-    parser = PDFParser(in_file)
-    doc = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    device = HTMLConverter(rsrcmgr, output_html, laparams=LAParams(line_overlap=0.1, line_margin=0.1, word_margin=0.05, char_margin=2.0))
-    string_device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    string_interpreter = PDFPageInterpreter(rsrcmgr, string_device)
+        for page in PDFPage.create_pages(doc):
+            interpreter.process_page(page)
 
-    for page in PDFPage.create_pages(doc):
-        interpreter.process_page(page)
+    return output_html.getvalue().decode('utf8').replace("<body>", '<body style="text-align: justify;">')
 
-    for page in PDFPage.create_pages(doc):
-        string_interpreter.process_page(page)
+def convert_pdf_to_string(path: Text) -> Text:
+    output_string = StringIO()
+    with open(path, 'rb') as in_file:
+        parser = PDFParser(in_file)
+        doc = PDFDocument(parser)
+        rsrcmgr = PDFResourceManager()
+        string_device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
+        string_interpreter = PDFPageInterpreter(rsrcmgr, string_device)
 
-print(output_html.getvalue().decode('utf8').replace("<body>", '<body style="text-align: justify;">'))
-print(output_string.getvalue())
+        for page in PDFPage.create_pages(doc):
+            string_interpreter.process_page(page)
+
+    print(output_string.getvalue())
